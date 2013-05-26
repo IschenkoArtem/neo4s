abbffc6cb8a60067f75b49da9cd88d5bbf41fd5c
IschenkoArtem <monty.joseph@gmail.com>
2013-05-26 20:34:23 +0400

    Comments for Connector.

--- src/main/scala/Connector.scala
+++ src/main/scala/Connector.scala
@@ -14,19 +14,38 @@ class Connector {
   val db = "db"
   val charset = "utf-8"
 
-  def node(id: Int, fullInfo: Boolean = false) = {
+  /**
+   * Return information about single node specified by id.
+   * @param id
+   * @return
+   */
+  def node(id: Int) = {
     val con: URLConnection = new URL(s"$host$db/data/node/$id").openConnection
     con.addRequestProperty("Accept", "application/json")
     println(con.getHeaderFields.toString)
     con.getInputStream
   }
 
+  /**
+   * Executes query as prepared statement with giving parameters.
+   * One should  prefer prepared statement parameters to literals in query as of performance considerations:
+   * Neo4j uses this parameters for profiling and query tuning.
+   * @param query cypher query as prepared statement
+   * @param params params for prepared statement
+   * @return
+   */
   def query(query: String, params: Map[String, String]) = {
     val joinedParams = params.map(p => "\"" + p._1 + "\":\"" + p._2 + "\"").mkString(",")
     val requestBody = s"""{ "query":"$query", "params":{$joinedParams} }"""
     val con = postConnection(s"$host$db/data/cypher",requestBody)
     read(con)
   }
+
+  /**
+   * Executes several statements in one transaction. Implementation uses "query" method..
+   * @param statements
+   * @return
+   */
   def statements(statements:Map[String,Map[String,String]]) = {
     val sts = statements.map(st=>{
       val query = st._1
@@ -38,6 +57,15 @@ class Connector {
     val con = postConnection(s"$host$db/data/transaction/commit",requestBody)
     read(con)
   }
+
+  /**
+   * Simple implementation of query execution. Because no streaming and continuous parsing is required this method
+   * reads hole response and returns single value stored in "data" represented as string.
+   * Usual usage is for query like "...RETURN count(...)"
+   * @param query
+   * @param params
+   * @return String
+   */
   def getSimpleResult(query: String, params: Map[String, String]) = {
     val result = Source.fromInputStream(this.query(query, params)).getLines().mkString("")
     val mapper = new ObjectMapper()
@@ -46,6 +74,12 @@ class Connector {
     println(mapper.writeValueAsString(value))
     value.data.head.head
   }
+
+  /**
+   * Gets input stream of connection and handles exceptions
+   * @param con Connection
+   * @return
+   */
   private def read(con:URLConnection) = {
     try {
       con.getInputStream
@@ -55,6 +89,13 @@ class Connector {
       }
     }
   }
+
+  /**
+   * Creates and opens connection for specified url and sends request body.
+   * @param url
+   * @param body - request body, should be JSON
+   * @return
+   */
   private def postConnection(url:String,body:String)={
     val con: URLConnection = new URL(url).openConnection
     con.setDoOutput(true)
--- src/test/scala/AppSpec.scala
+++ src/test/scala/AppSpec.scala
@@ -9,20 +9,17 @@ import scala.io.Source
 class AppSpec extends FlatSpec with Matchers {
 
   "Connector.node(0)" should "return information about root node" in {
-    Source.fromInputStream(new Connector().node(0)).getLines().mkString("").replaceAll("\\s+", "") should include ("\"data\":{}")
+    Source.fromInputStream(new Connector().node(0)).getLines().mkString("").replaceAll("\\s+", "") should include("\"data\":{}")
   }
   "Connector.query(\"START a = node(0) RETURN a\", Map())" should "return information about root node" in {
-    Source.fromInputStream(new Connector().query("START a = node(0) RETURN a", Map())).getLines().mkString("") should include ("\"data\":{}")
+    Source.fromInputStream(new Connector().query("START a = node(0) RETURN a", Map())).getLines().mkString("") should include("\"data\":{}")
   }
   "Connector.statements()" should "return information about root node" in {
-    val sts:Map[String,Map[String,String]] = Map("START n = node(0) RETURN n" -> Map())
+    val sts: Map[String, Map[String, String]] = Map("START n = node(0) RETURN n" -> Map())
     val response = Source.fromInputStream(new Connector().statements(sts)).getLines().mkString("")
-
-    response should include ("\"data\":[[{}]]")
+    response should include("\"data\":[[{}]]")
   }
-
-  "Connector.getSimpleResult()" should "return number of root nodes(wish is 1)" in{
+  "Connector.getSimpleResult()" should "return number of root nodes(wish is 1)" in {
     new Connector().getSimpleResult("START a = node(0) RETURN count(a)", Map()) should equal("1")
   }
-
 }
